78589e51d632ac14813e2dd3637b9f553066a4c7
thanos <atha0043@edu.eal.dk>
2016-05-26 11:59:53 +0200

    Project2

--- DaxiomaticWebPage/Controllers/HomeController.cs
+++ DaxiomaticWebPage/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DaxiomaticWebPage.Models;
 
 namespace DaxiomaticWebPage.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
         {
             return View();
@@ -24,6 +26,41 @@ namespace DaxiomaticWebPage.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        // This is a get method for Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        // This is a post Method for Login
+        [HttpPost]
+        public ActionResult Login(Models.LoginInfo daxLogin)
+        {
+
+            using (DaxiomaticWebPage.EntityFrameWork.villadsenwp_dk_dbEntities myDB = new EntityFrameWork.villadsenwp_dk_dbEntities())
+            {
+                var usr = myDB.DaxLoginInfo.Where(u => u.Username == daxLogin.UserName && u.Password == daxLogin.PassWord).FirstOrDefault();
+
+                if (usr.UserLevel == 0)
+                {
+                    SessionData.SessionProp = usr.ID;
+
+                    Session["UserName"] = usr.Username.ToString();
+                    return RedirectToAction("StatisticsUserIndex", "UserStatistics");
+                }
+                else if (usr.UserLevel == 1)
+                {
+                    SessionData.SessionProp = usr.ID;
+                    Session["UserName"] = usr.Username.ToString();
+                    return RedirectToAction("AdminStatisticIndex", "AdminStatistic");
+                }
+                else if (usr != null)
+                {
+                    ModelState.AddModelError("", "UserName or PassWord is not valid");
+                }
+            }
             return View();
         }
     }
---
+++ DaxiomaticWebPage/Models/LoginInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DaxiomaticWebPage.Models
+{
+    public class LoginInfo
+    {
+        [Key]
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+        //public int UserLevel { get; set; }
+    }
+}
