c9ff821de0325a54f016e82670cd54e86be5fbb9
thanos <atha0043@edu.eal.dk>
2016-05-27 00:22:05 +0200

    Working

---
+++ DaxiomaticWebPage/Models/Tables/CompanyData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DaxiomaticWebPage.Models.Tables
+{
+    public class CompanyData
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int InputID { get; set; }
+
+        public CompanyData(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
